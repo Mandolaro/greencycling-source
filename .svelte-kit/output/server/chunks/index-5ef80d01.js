15cc9bdde564ace6b27904d4c90464fd222686a8
Mandolaro <62208322+Mandolaro@users.noreply.github.com>
2021-12-20 14:45:58 +0700

    Revert "Codebase for Greencycling"
    This reverts commit d9c8b2ce26a98f5fb72444f2b6474c440fb9c8ef.

--- .svelte-kit/output/server/chunks/index-5ef80d01.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "./app-c055211a.js";
-import "@sveltejs/kit/ssr";
-const Buyer = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="${"h-screen flex"}"><div class="${"m-auto text-4xl"}">Work in progress...
-    </div></div>`;
-});
-export { Buyer as default };
